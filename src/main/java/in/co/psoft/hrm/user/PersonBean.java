aee071a9469576f211b4332296a7ebe2eaff2ad3
Peppin Shaju <PL00362599@TechMahindra.com>
2018-04-24 10:24:00 +0530

    Person bundle

---
+++ src/main/java/in/co/psoft/hrm/user/PersonBean.java
@@ -0,0 +1,72 @@
+package in.co.psoft.hrm.user;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import in.co.psoft.hrm.bone.spring.RequestScopedComponent;
+import in.co.psoft.hrm.domain.User;
+import in.co.psoft.hrm.repo.UserRepo;
+
+@ManagedBean(name = "personBean")
+@RequestScopedComponent
+public class PersonBean {
+
+	@Autowired
+	private UserRepo userRepo;
+
+	@PersistenceContext
+	private EntityManager em;
+
+	private List<User> users;
+
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@PostConstruct
+	public void init() {
+		user = new User();
+	}
+
+	public String saveUser() {
+		em.persist(user);
+		return "listUser";
+	}
+
+	public List<User> getUsers() {
+		if (users == null) {
+			users = userRepo.findAll();
+		}
+		return users;
+	}
+
+	public String findById(Long id) {
+		user = userRepo.findById(id);
+		return "updatePerson";
+	}
+
+	public String updateUser() {
+		em.merge(user);
+		return "listUser";
+	}
+	
+	public String deleteUser(Long id) {
+		User user = em.find(User.class, id);
+		System.out.println(user.getId());
+		em.remove(user);
+		return null;
+	}
+
+}
